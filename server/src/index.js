2831fa3bcbc05d6dfd7ae627d97400f8a13be05c
lachldwaws <lachldw@amazon.com>
2022-02-27 08:42:49 +0800

    Added MySQL connector to server, moved some compose arguments to secrets, and supplied compose with a secrets file to load secrets into containers

--- server/src/index.js
+++ server/src/index.js
@@ -4,8 +4,21 @@ const MySQL = require('mysql');
 const app = Express();
 const port = 5000;
 
+const connection_parameters = {
+    host: "database",
+    user: "root",
+    password: process.env.MYSQL_ROOT_PASSWORD
+};
+
+var db = MySQL.createConnection(connection_parameters);
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`);
+    console.log(process.env);
+    db.connect(err => {
+        if (err) throw Error(`Error during connection: ${err}\n`);
+        else console.log("Connected to MySQL!\n");
+    });
 })
 
 app.get('/', (req, res) => {
