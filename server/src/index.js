bb145c7f132cfc63143bfc716f8ea21d013282e3
lachldwaws <lachldw@amazon.com>
2022-02-27 09:34:51 +0800

    Added a simple route to get all users from the database

--- server/src/index.js
+++ server/src/index.js
@@ -7,19 +7,32 @@ const port = 5000;
 const connection_parameters = {
     host: "database",
     user: "root",
-    password: process.env.MYSQL_ROOT_PASSWORD
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: 'website'
 };
 
 var db = MySQL.createConnection(connection_parameters);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`);
-    console.log(process.env);
     db.connect(err => {
         if (err) throw Error(`Error during connection: ${err}\n`);
         else console.log("Connected to MySQL!\n");
     });
 })
+
+/* CRUD Routes */
+
+// Read all the entries in the users table.
+
+app.get('/users', (req, res) => {
+    db.query("SELECT * FROM users;", (err, result, fields) => {
+        console.log("Querying all users.");
+        res.status(err ? 500 : 200).send(err ? err : result);
+    });
+});
+
+/* Other Routes */
 
 app.get('/', (req, res) => {
     console.log("This is the homepage.");
