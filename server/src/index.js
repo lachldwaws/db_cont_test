734a8cc1bf52c2637574e8556edad6c362f922e8
lachldwaws <lachldw@amazon.com>
2022-02-28 15:18:34 +0800

    Added functionality to change user details. Time to write tests; git push

--- server/src/index.js
+++ server/src/index.js
@@ -36,7 +36,9 @@ app.listen(port, () => {
 // Add a user to the users table.
 // Accepted parameters: ['username', 'email', 'password_hash']
 // Password hash must be bcrypt, this can be verified by checking if it starts with "$2" (this is a weak check).
-
+validPasswordHash = password_hash => {
+    return (password_hash.startsWith("$2") && password_hash.length == 60);
+}
 /* Test Working Command:
 
 curl --header "Content-Type: application/json" \
@@ -64,14 +66,14 @@ app.put('/users/add', (req, res) => {
             // Checking email (this is nested because db queries are async)
             if (email) {
                 console.log(`Checking email ${email}...`);
-                if (!(isemail.validate(email, { allowUnicode: false }) && email.length <= 254)) {
+                if (!(isemail.validate(email, { allowUnicode: false }))) {
                     return res.status(400).send("Email address is invalid. This is normally due to the fact that it is either too long, malformed, or contains non-ASCII characters.");
                 }
             } else console.log("No email provided. Skipping email check...");
         
             if (password_hash) {
                 console.log(`Checking password hash...`);
-                if (!(password_hash.startsWith("$2") && password_hash.length == 60)) {
+                if (!validPasswordHash(password_hash)) {
                     return res.status(400).send("Malformed password hash.");
                 }
             } else return res.status(400).send("Required field \"password_hash\" not found.");
@@ -117,13 +119,69 @@ app.get('/users/find', (req, res) => {
     } else res.status(400).send("Bad request, no username parameter found in body.")
 });
 
-// Update a user's information. It is only possible to update a user's username, email address, or password hash.
-// Expect 2 key-value pairs in the form. First pair is "user_id" / id (hidden). Second pair is field_name / new_value.
+// Update a user's information. It is only possible to update a user's username, email, or password_hash.
+// Expect 3 key-value pairs in the following form:
+//  - Pair 1: "user_id": id (hidden field in form)
+//  - Pair 2: "field_name": name of the field containing the value to be changed
+//  - Pair 3: "field_value": the new value of this field
 
+/* Test Command:
+
+curl --header "Content-Type: application/json" \
+--request PATCH \
+--data '{"user_id": 1, "field_name": "username", "field_value": "stillawesome123"}' \
+http://localhost:9001/users/update
+
+*/
 app.patch('/users/update', (req, res) => {
-    // TODO
-    res.status(501).send("To be implemented.");
+    const user_id = req.body["user_id"];
+    const field_name = req.body["field_name"];
+    const field_value = req.body["field_value"]
+    if (!(user_id && field_name && field_value)) return res.status(400).send("User update request missing 1 or more required fields in request body.");
+
+    // Validate user ID
+    db.query("SELECT id FROM users WHERE id = ?", [user_id], (err, result, fields) => {
+        if (err) return res.status(500).send("Error checking user_id validity.");
+        else if (result.length == 0) return res.status(400).send(`No user with ID ${user_id}.`);
+
+        /* Validating and updating the new value */
+        if (field_name == "username") {
+            db.query("SELECT username FROM users WHERE username = ?;", [field_value], (err, result, fields) => {
+                if (err) return res.status(500).send("Error checking username validity.");
+                else if (result.length > 0) return res.status(400).send(`Username ${username} is taken.`);
+
+                // Insert username into database here since within async call.
+                db.query("UPDATE users SET username = ? WHERE id = ?", [field_value, user_id], (err, result, fields) => {
+                    if (err) return res.status(500).send("Error updating username.");
+                    else return res.status(200).send(`${field_name} ${field_value} updated successfully.`);
+                })
+
+            })
+        } else if (field_name == "email") {
+            if (!isemail.validate(field_value, { allowUnicode: false }))
+                return res.status(400).send("Email address is invalid. This is normally due to the fact that it is either too long, malformed, or contains non-ASCII characters.");
+        } else if (field_name == "password_hash") {
+            if (!validPasswordHash(field_value))
+                return res.status(400).send("Malformed password hash.");
+        } else return res.status(400).send(`Unknown field name ${field_name}.`);
+
+        // Checks passed, insert value for email or password_hash into database.
+        if (field_name == "username") return;
+        let query_string = "";
+        if (field_name == "email") query_string = "UPDATE users SET email = ? WHERE id = ?;";
+        else query_string = "UPDATE users SET password_hash = ? WHERE id = ?;";
+        db.query(query_string, [field_value, user_id], (err, result, fields) => {
+            if (err) return res.status(500).send(`Error updating ${field_name} value.`);
+            // DO NOT display the password_hash.
+            else return field_name == "password_hash" ?
+                        res.status(200).send(`${field_name} updated successfully.`) :
+                        res.status(200).send(`${field_name} ${field_value} updated successfully.`);
+        });
+
+    });
+
 });
+
 
 // Delete a user from the users table. Requires the username of the user to be deleted.
 
