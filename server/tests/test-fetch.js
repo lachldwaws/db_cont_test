07ae08dd3b1b7d52804330c9ae081a8a35b55b5d
lachldwaws <lachldw@amazon.com>
2022-03-06 13:22:40 +0800

    Replaced node-fetch with axios for fetching from server

--- server/tests/test-fetch.js
+++ server/tests/test-fetch.js
@@ -1,18 +1,22 @@
-// We are using node-fetch@2 - Check the documentation below for more details.
-// https://www.npmjs.com/package/node-fetch
-// This is a simple script to practise using fetch.
-const fetch = require('node-fetch');
+// Axios documentation: https://www.npmjs.com/package/axios
+const axios = require('axios').default;
 
-const testFetch = async (url) => {
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-    return response;
-}
-
-testFetch("https://www.forsakenidol.com/").then(response => {
-    console.log(response);
+axios({
+    method: 'GET',
+    url: 'http://localhost:9001/users/find',
+    headers: {'Content-Type': 'application/json'},
+    data: {
+        username: "greatestuser789"
+    }
+}).then(response => {
+    // This block is executed for all 2xx response codes.
+    console.log(response.status);
+    console.log(response.data);
+}).catch(error => {
+    // Errors occur when the server responds with a status code outside of the 2xx range.
+    // Our server has a few options that return 4xx and 5xx codes, so they will end up here.
+    // This is explained here: https://www.npmjs.com/package/axios#handling-errors
+    console.log("Error!");
+    console.log(error.response.status);
+    console.log(error.response.data);
 });
